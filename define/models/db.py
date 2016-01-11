6fdee18f6e52d1681aafac441db69b14648a9012
Antonio Nunez <antonio.nunez.311@gmail.com>
2016-01-11 03:27:20 -0600

    Define
    Se soluciono el problema de actualización al crear nuevos grupos.
    
    Se agrego el sistema de membresías para el manejo de los usuarios
    administradores.
    
    Solución a errores en los modos particular y general.

---
+++ define/models/db.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+
+#########################################################################
+## This scaffolding model makes your app work on Google App Engine too
+## File is released under public domain and you can use without limitations
+#########################################################################
+
+## if SSL/HTTPS is properly configured and you want all HTTP requests to
+## be redirected to HTTPS, uncomment the line below:
+# request.requires_https()
+
+## app configuration made easy. Look inside private/appconfig.ini
+from gluon.contrib.appconfig import AppConfig
+## once in production, remove reload=True to gain full speed
+myconf = AppConfig(reload=True)
+
+
+if not request.env.web2py_runtime_gae:
+    ## if NOT running on Google App Engine use SQLite or other DB
+    db = DAL(myconf.take('db.uri'), pool_size=myconf.take('db.pool_size', cast=int), check_reserved=['all'])
+else:
+    ## connect to Google BigTable (optional 'google:datastore://namespace')
+    db = DAL('google:datastore+ndb')
+    ## store sessions and tickets there
+    session.connect(request, response, db=db)
+    ## or store session in Memcache, Redis, etc.
+    ## from gluon.contrib.memdb import MEMDB
+    ## from google.appengine.api.memcache import Client
+    ## session.connect(request, response, db = MEMDB(Client()))
+
+## by default give a view/generic.extension to all actions from localhost
+## none otherwise. a pattern can be 'controller/function.extension'
+response.generic_patterns = ['*'] if request.is_local else []
+## choose a style for forms
+response.formstyle = myconf.take('forms.formstyle')  # or 'bootstrap3_stacked' or 'bootstrap2' or other
+response.form_label_separator = myconf.take('forms.separator')
+
+
+## (optional) optimize handling of static files
+# response.optimize_css = 'concat,minify,inline'
+# response.optimize_js = 'concat,minify,inline'
+## (optional) static assets folder versioning
+# response.static_version = '0.0.0'
+#########################################################################
+## Here is sample code if you need for
+## - email capabilities
+## - authentication (registration, login, logout, ... )
+## - authorization (role based authorization)
+## - services (xml, csv, json, xmlrpc, jsonrpc, amf, rss)
+## - old style crud actions
+## (more options discussed in gluon/tools.py)
+#########################################################################
+
+from gluon.tools import Auth, Service, PluginManager
+
+auth = Auth(db)
+service = Service()
+plugins = PluginManager()
+
+## create all tables needed by auth if not custom tables
+auth.define_tables(username=False, signature=False)
+
+## configure email
+mail = auth.settings.mailer #Ajustes del mail
+mail.settings.server = 'smtp.gmail.com:587' #Servidor que se usará
+mail.settings.sender = 'define.gil@gmail.com' #Correo desde el cual se enviarán las notificaciones
+mail.settings.login = 'define.gil:defineadmin' #Datos de acceso del correo
+
+## configure auth policy
+auth.settings.registration_requires_verification = True
+auth.settings.login_after_registration = False
+auth.settings.registration_requires_approval = False
+auth.settings.reset_password_requires_verification = True
+auth.messages.verify_email = 'Click on the link http://' +     request.env.http_host +     URL(r=request,c='default',f='user',args=['verify_email']) +     '/%(key)s to verify your email'
+auth.messages.reset_password = 'Click on the link http://' +     request.env.http_host +     URL(r=request,c='default',f='user',args=['reset_password']) +     '/%(key)s to reset your password'
+
+#########################################################################
+## Define your tables below (or better in another model file) for example
+##
+## >>> db.define_table('mytable',Field('myfield','string'))
+##
+## Fields can be 'string','text','password','integer','double','boolean'
+##       'date','time','datetime','blob','upload', 'reference TABLENAME'
+## There is an implicit 'id integer autoincrement' field
+## Consult manual for more options, validators, etc.
+##
+## More API examples for controllers:
+##
+## >>> db.mytable.insert(myfield='value')
+## >>> rows=db(db.mytable.myfield=='value').select(db.mytable.ALL)
+## >>> for row in rows: print row.id, row.myfield
+#########################################################################
+
+## after defining tables, uncomment below to enable auditing
+# auth.enable_record_versioning(db)
+
+
+from gluon.tools import Auth
+
+auth = Auth(db)
+db.define_table('termino', Field('ter', 'text'))
+db.define_table('definicion', Field('defi', 'text'), Field('termino_id', 'reference termino', default=1))
+db.define_table('grupo', Field('grup'), Field('termino_id', 'reference termino'), Field('tipo'), Field('modo'))
+db.define_table('relacion', Field('keywords'), Field('grado'), Field('grupo_id', 'reference grupo'), Field('definicion_id', 'reference definicion'), Field('usuario', 'reference auth_user'), Field('modo'))
+db.define_table('temporal', Field('usuario_id', 'reference auth_user'), Field('definicion_id', 'reference definicion'), Field('modo'))
+
+
+usuario = auth.user_id
+user = auth.user
+
+def name_of(usuario):
+    return '%(first_name)s %(last_name)s' % usuario
